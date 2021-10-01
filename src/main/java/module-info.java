23f6ae4ce2d9c2c21cba7532ad5c427c674b2ead
csc530 <cscousin@lakeheadu.ca>
2021-10-01 12:28:53 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.graphical {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.web;
+	
+	requires org.controlsfx.controls;
+	requires com.dlsc.formsfx;
+	requires validatorfx;
+	requires org.kordamp.ikonli.javafx;
+	requires org.kordamp.bootstrapfx.core;
+	requires eu.hansolo.tilesfx;
+	
+	opens com.example.graphical to javafx.fxml;
+	exports com.example.graphical;
+	exports com.example.graphical.Models;
+	opens com.example.graphical.Models to javafx.fxml;
+}
