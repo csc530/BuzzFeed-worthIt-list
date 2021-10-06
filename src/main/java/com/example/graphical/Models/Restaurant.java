df57d95a27eac219da0f5780b9acc7f8358b3979
csc530 <cscousin@lakeheadu.ca>
2021-10-06 16:34:09 -0400

    Created instance variables for the model class.

---
+++ src/main/java/com/example/graphical/Models/Restaurant.java
@@ -0,0 +1,12 @@
+package com.example.graphical.Models;
+
+import java.util.Date;
+
+public class Restaurant{
+	private String name,category;
+	private int id,locations;
+	private boolean active;
+	private double value;
+	private Date established;
+
+}
