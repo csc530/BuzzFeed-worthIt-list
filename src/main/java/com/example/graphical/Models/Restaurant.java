79cfa3d0c3f8f00df3010cc6cf1ba1a54946915f
csc530 <cscousin@lakeheadu.ca>
2021-10-06 21:12:32 -0400

    Created some constructors for a restaurant

--- src/main/java/com/example/graphical/Models/Restaurant.java
+++ src/main/java/com/example/graphical/Models/Restaurant.java
@@ -1,12 +1,36 @@
 package com.example.graphical.Models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Restaurant{
-	private String name,category;
-	private int id,locations;
+	public static ArrayList<String> CategoryList = (ArrayList<String>) Arrays.asList(new String[]{"Diner", "Fast food", "Fancy", "Other", "Unspecified"});
+	private String name, category;
+	private int id, locations;
 	private boolean active;
 	private double value;
 	private Date established;
-
+	
+	public Restaurant(String name, String category, int id, int locations, boolean active, double value, Date established){
+		this.name = name;
+		this.category = category;
+		this.id = id;
+		this.locations = locations;
+		this.active = active;
+		this.value = value;
+		this.established = established;
+	}
+	
+	public Restaurant(String name, boolean active, int locations, Date established){
+		this.name = name;
+	}
+	public Restaurant(String name, boolean active, int locations, Date established, String category){
+		this(name, active,locations, established);
+	}
+	public Restaurant(String name, boolean active, int locations, Date established, String category, double value){
+		
+		this(name, active,locations, established, category);
+	}
+	
 }
