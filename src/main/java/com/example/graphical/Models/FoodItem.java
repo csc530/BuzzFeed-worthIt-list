816f861939a57b6c267348029780e738c335ad1f
csc530 <cscousin@lakeheadu.ca>
2021-10-11 13:40:04 -0400

    Revamped Restaurant class for the data I'll be using also created a foodItem class for the data I'll be using

---
+++ src/main/java/com/example/graphical/Models/FoodItem.java
@@ -0,0 +1,12 @@
+package com.example.graphical.Models;
+
+public class FoodItem{
+	private String name;
+	private double price;
+	private int pricePoint;
+	
+	public FoodItem(String name, double price){
+		this.name = name;
+		this.price = price;
+	}
+}
--- src/main/java/com/example/graphical/Models/Restaurant.java
+++ src/main/java/com/example/graphical/Models/Restaurant.java
@@ -6,31 +6,18 @@ import java.util.Date;
 
 public class Restaurant{
 	public static ArrayList<String> CategoryList = (ArrayList<String>) Arrays.asList(new String[]{"Diner", "Fast food", "Fancy", "Other", "Unspecified"});
-	private String name, category;
-	private int id, locations;
-	private boolean active;
-	private double value;
-	private Date established;
+	private String name, notes, country, city;
+	private int id, season, episode;
+	private ArrayList<FoodItem> foodItems;
 	
-	public Restaurant(String name, String category, int id, int locations, boolean active, double value, Date established){
+	public Restaurant(String name, String notes, String country, String city, int id, int season, int episode, ArrayList<FoodItem> foodItems){
 		this.name = name;
-		this.category = category;
+		this.notes = notes;
+		this.country = country;
+		this.city = city;
 		this.id = id;
-		this.locations = locations;
-		this.active = active;
-		this.value = value;
-		this.established = established;
+		this.season = season;
+		this.episode = episode;
+		this.foodItems = foodItems;
 	}
-	
-	public Restaurant(String name, boolean active, int locations, Date established){
-		this.name = name;
-	}
-	public Restaurant(String name, boolean active, int locations, Date established, String category){
-		this(name, active,locations, established);
-	}
-	public Restaurant(String name, boolean active, int locations, Date established, String category, double value){
-		
-		this(name, active,locations, established, category);
-	}
-	
 }
