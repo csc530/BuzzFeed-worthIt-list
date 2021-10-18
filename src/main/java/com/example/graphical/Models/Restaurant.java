67990c45d83d550ee87bd5bca037496ab8a58ada
csc530 <cscousin@lakeheadu.ca>
2021-10-18 08:51:41 -0400

    Added validation for Restaurant.java

--- src/main/java/com/example/graphical/Models/Restaurant.java
+++ src/main/java/com/example/graphical/Models/Restaurant.java
@@ -8,15 +8,55 @@ public class Restaurant{
 	private ArrayList<FoodItem> foodItems;
 	
 	public Restaurant(String season, int episode, String episodeName, String name,String city,String country, String notes){
-		this.name = name;
-		this.notes = notes;
-		this.country = country;
-		this.city = city;
-		this.episodeName = episodeName;
-		this.season = season;
-		this.episode = episode;
+		setCity(city);
+		setName(name);
+		setNotes(notes);
+		setCountry(country);
+		setEpisode(episode);
+		setEpisodeName(episodeName);
+		setSeason(season);
 		foodItems = new ArrayList<FoodItem>();
  	}
+	
+	public void setName(String name){
+		if(name.length()<3)
+			throw new IllegalArgumentException("Name must be at least 3 characters long.");
+		this.name = name;
+	}
+	
+	public void setNotes(String notes){
+		if(notes == null)
+		throw new IllegalArgumentException("Notes cannot be null.");
+		this.notes = notes;
+	}
+	
+	public void setCountry(String country){
+		if(country == null)
+			throw new IllegalArgumentException("Country cannot be null.");
+		this.country = country;
+	}
+	
+	public void setCity(String city){
+		if(city == null)
+		throw new IllegalArgumentException("city cannot be null.");
+		this.city = city;
+	}
+	
+	public void setEpisodeName(String episodeName){
+		if(episodeName == null)
+			throw new IllegalArgumentException("The episode name cannot be null.");
+		this.episodeName = episodeName;
+	}
+	
+	public void setSeason(String season){
+		if(season == null)
+			throw new IllegalArgumentException("season name cannot be null.");
+		this.season = season;
+	}
+	
+	public void setEpisode(int episode){
+		this.episode = episode;
+	}
 	
 	public String getName(){
 		return name;
@@ -58,7 +98,7 @@ public class Restaurant{
 		foodItems.add(item);
 	}
 	
-	public void addItem(ArrayList<FoodItem> item){
+	public void addItems(ArrayList<FoodItem> item){
 		foodItems.addAll(item);
 	}
 }
